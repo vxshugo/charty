42b8e98047c800ac1bb286e531169ddbf7513ae5
Your Name <you@example.com>
2022-11-25 16:45:22 +0600

    Test

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import Hero from "./component/hero";
 import Mission from "./component/mission";
 import About from "./component/about";
 import Case from "./component/case";
+import {useEffect, useState} from "react";
 
 
 function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+    },[])
+
   return (
     <div className="App">
-        <PreLoader/>
-        <TopNav/>
-        <Hero/>
-        <Mission/>
-        <About/>
-        <Case/>
+        {loading ? (
+            <div>
+                <TopNav/>
+                <Hero/>
+                <Mission/>
+                <About/>
+                <Case/>
+            </div>
+        ): (
+            <PreLoader/>
+        )}
     </div>
   );
 }
